7ea07c098786ade167a163f987a13e70e4dcc195
Jesús Martín Fernández <txus.martin.fernandez@gmail.com>
2025-09-20 11:55:04 +0200

    Initial version

---
+++ src/Schemas/Data.js
@@ -0,0 +1,270 @@
+export const Data = {
+  profile: {
+    name: "Jesús Martín",
+    ocupation: "Automation software developer",
+    location: "Valencia, España",
+    email: "txus.martin.fernandez@gmail.com",
+    telephone: "+34 675407600",
+    image: "images/me.jpg",
+  },
+  aboutMe: {
+    label: "Perfil",
+    description:
+      "Programador, capacitado para desarrollar aplicaciones de automatización e informáticas en diversas tecnologías y múltiples lenguajes de programación. \n\n Entusiasta de la programación y la electrónica.",
+  },
+  skills: {
+    technicalLabel: "Tecnologías",
+    softLabel: "Skills",
+    languageLabel: "Idiomas",
+    technicalSkills: [
+      "TIA Portal",
+      "Step 7",
+      "WinCC",
+      "WinCC Unified,",
+      "RSLogix",
+      "FactoryTalk",  
+      "OPC-UA",    
+      "C,C++,C#",
+      "WPF",
+      "Docker",
+      "React",
+      "Java",
+      "Nodejs",            
+      "SQL",
+      "Git",
+    ],
+    softSkills: [
+      "Capacidad de análisis",
+      "Resolución de Problemas",
+      "Pensamiento Crítico",
+      "Trabajo en equipo",
+      "Comunicación",
+      "Proactividad",
+      "Creatividad",
+      "Autodidacta",
+    ],
+    languageSkills: [
+      "Castellano: Alto",
+      "Ingles: Medio",
+      "Frances: Básico",
+    ],
+  },
+  socialMedia: {
+    label: "SOCIAL",
+    social: [
+      {
+        label: "Visitar el perfil de Jesús Martín en LinkedIn",
+        name: "linkedin",
+        url: "linkedin.com/in/jmartin-dev",
+        className: "bxl-linkedin-square",
+      },
+      {
+        label: "Visitar el perfil de Jesús Martín en GitHub",
+        name: "github",
+        url: "github.com/J-Martin-Dev",
+        className: "bxl-github",
+      },
+    ],
+  },
+  experience: {
+    works: [
+      {
+        title: "Ingeniero de control",
+        period: "2007.10 - 2013.6",
+        company: "Mecalux S.A",
+        description: [
+          "Programación de maquinaria destinada a la almaceneria y manejo de cargas.",
+          "Desarrollo de aplicaciones en Step 7 y entornos de usuario en WinCC Flexible.",
+          "Desarrollo de aplicaciones y entornos de usuario en soft-plc propietario.",
+          "Servicio de asistencia post-venta.",
+        ],
+      },
+      {
+        title: "Ingeniero de control",
+        period: "2013.6 - 2014.12",
+        company: "Autónomo",
+        description: [
+          "Programación de maquinaria destinada a la almaceneria y manejo de cargas.",
+          "Desarrollo de aplicaciones en Step 7 y entornos de usuario en WinCC Flexible.",
+        ],
+      },
+      {
+        title: "Ingeniero de proyectos",
+        period: "2015.1 - 2019.12",
+        company: "Órbita Ingenieria S.L.",
+        description: [
+          "Programación de maquinaria para el sector del automovil, cosmético y alimentación.",
+          "Desarrollo de aplicaciones en TIA Portal, RSLogix.",
+          "Desarrollo de entornos de usuario en WinCC y FactoryTalk.",
+          "Desarrollo y mantenimiento de aplicaciones para disposivos embebidos con Windows CE.",
+          "Servicio de asistencia post-venta.",
+        ],
+      },
+      {
+        title: "Responsable técnico",
+        period: "2020.1 - 2021.4",
+        company: "Órbita Ingenieria S.L.",
+        description: [
+          "Gestión de departamento de programación y asesoramiento técnico en ofertas.",
+          "Servicio de asistencia post-venta.",
+        ],
+      },
+      {
+        title: "Ingeniero de control",
+        period: "2021.5 - Presente",
+        company: "GH Induction",
+        description: [
+          "Programación de maquinaria industrial para procesos de inducción.",
+          "Desarrollo de aplicaciones en TIA Portal, RSLogix y FactoryTalk.",          
+          "Desarrollo y mantenimiento de aplicaciones de monitorización de procesos.",
+          "Servicio de asistencia post-venta.",
+        ],
+      },
+    ],
+    academic: [
+      {
+        career: "Ingeniería técnica en telecomunicaciones, esp. Sistemas Electrónicos",
+        date: "2007",
+        institution: "Universidad de Valencia",
+      },
+    ],
+    proyects: [
+      {
+        name: "Orliman - La pobla de Vallbona",
+        company: "Mecalux S.A.",
+        period: "2007.10 - 2007.12",
+        description: [
+          "Puesta en marcha de almacén automático de pequeñas cargas.",
+        ],
+      },
+      {
+        name: "Ubesol - Adzeneta de Albaida",
+        company: "Mecalux S.A.",
+        period: "2008.1 - 2008.7",
+        description: [
+          "Desarrollo y puesta en marcha de almacén automático de grandes cargas.",          
+        ],
+      },
+      {
+        name: "Damel - Paterna",
+        company: "Mecalux S.A.",
+        period: "2008.8 - 2008.10",
+        description: [
+          "Desarrollo y puesta en marcha de buffer de macerado para productos alimenticios.",
+        ],
+      },
+      {
+        name: "Porcelanosa (Ceranco) - Villarreal",
+        company: "Mecalux S.A.",
+        period: "20XX.xx - 20xx.xx",
+        description: [
+          "Desarrollo y puesta en marcha de almacén automático de grandes cargas.",
+        ],
+      },
+      {
+        name: "Porcelanosa (L'Antic Colonial) - Villarreal",
+        company: "Mecalux S.A.",
+        period: "20xx.xx - 20xx.xx",
+        description: [
+          "Desarrollo y puesta en marcha de almacén automático de grandes cargas.",          
+        ],
+      },
+      {
+        name: "Tempe (Grupo Inditex) - Elche",
+        company: "Mecalux S.A.",
+        period: "20xx.xx - 20xx.xx",
+        description: [
+          "Retrofit de almacén automático de grandes cargas.",          
+        ],
+      },
+      {
+        name: "Idaho State Liquour Division - Idaho",
+        company: "Mecalux-Interlake",
+        period: "20xx.xx - 20xx.xx",
+        description: [
+          "Desarrollo y puesta en marcha de almacén automático de grandes cargas.",          
+        ],
+      },
+      {
+        name: "Simpta Pieces - Francia",
+        company: "Mecalux S.A.",
+        period: "20xx.xx - 20xx.xx",
+        description: [
+          "Desarrollo y puesta en marcha de almacén vertical.",          
+        ],
+      },
+      {
+        name: "Saint Louis Sucre - Francia",
+        company: "Autónomo (Mecalux S.A.)",
+        period: "20xx.xx - 20xx.xx",
+        description: [
+          "Desarrollo y puesta en marcha de grandes cargas.",          
+        ],
+      },
+      {
+        name: "President - Rusia",
+        company: "Autónomo (Mecalux S.A.)",
+        period: "20xx.xx - 20xx.xx",
+        description: [
+          "Desarrollo y puesta en marcha de grandes cargas.",          
+        ],
+      },
+      {
+        name: "Ford Almussafes - Valencia",
+        company: "Órbita Ingeniería S.L.",
+        period: "20xx.xx - 20xx.xx",
+        description: [
+          "Desarrollo y puesta en marcha de diversas máquinas de montaje, mecanizado y test de fugas para la planta de fabricación de motores.",          
+        ],
+      },
+      {
+        name: "Seat Martorell - Martorell",
+        company: "Órbita Ingeniería S.L.",
+        period: "20xx.xx - 20xx.xx",
+        description: [
+          "Retrofit de proceso de cataforesis.",          
+        ],
+      },
+      {
+        name: "CIE Amaya - México",
+        company: "GH Induction",
+        period: "20xx.xx - 20xx.xx",
+        description: [
+          "Máquina de temple y revenido de tulipas.",          
+        ],
+      },
+      {
+        name: "Induction Process Monitor - Valencia",
+        company: "GH Induction",
+        period: "20xx.xx - 20xx.xx",
+        description: [          
+          "Desarrollo de aplicación de monitorización de procesos de inducción.",          
+        ],
+      },
+      {
+        name: "FANASA - Mexico",
+        company: "GH Induction",
+        period: "20xx.xx - 20xx.xx",
+        description: [
+          "Máquina de temple de cubos de ruedas.",       
+        ],
+      },
+      {
+        name: "RollRite - EEUU",
+        company: "GH Induction",
+        period: "20xx.xx - 20xx.xx",
+        description: [
+          "Máquina de temple de ejes.",          
+        ],
+      },
+      {
+        name: "GKN Alamance - EEUU",
+        company: "GH Induction",
+        period: "20xx.xx - 20xx.xx",
+        description: [
+          "Máquina de temple de tulipas.",          
+        ],
+      },
+    ],
+  },
+};
