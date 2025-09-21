e59a251816616031a736307045ccd56bfcbb5820
Jesús Martín Fernández <txus.martin.fernandez@gmail.com>
2025-09-21 11:44:39 +0200

    Update photo and data

--- src/Schemas/Data.js
+++ src/Schemas/Data.js
@@ -86,7 +86,7 @@ export const Data = {
         company: "GH Induction",
         description: [
           "Programación de maquinaria industrial para procesos de inducción.",
-          "Desarrollo de aplicaciones en TIA Portal, RSLogix y FactoryTalk.",          
+          "Desarrollo de aplicaciones de automatización.",         
           "Desarrollo y mantenimiento de aplicaciones de monitorización de procesos.",
           "Servicio de asistencia post-venta.",
         ],
@@ -106,9 +106,8 @@ export const Data = {
         company: "Órbita Ingenieria S.L.",
         description: [
           "Programación de maquinaria para el sector del automovil, cosmético y alimentación.",
-          "Desarrollo de aplicaciones en TIA Portal, RSLogix.",
-          "Desarrollo de entornos de usuario en WinCC y FactoryTalk.",
-          "Desarrollo y mantenimiento de aplicaciones para disposivos embebidos con Windows CE.",
+          "Desarrollo de aplicaciones de automatización.",          
+          "Desarrollo y mantenimiento de aplicaciones para disposivos embebidos (Windows CE).",
           "Servicio de asistencia post-venta.",
         ],
       },
@@ -118,7 +117,7 @@ export const Data = {
         company: "Autónomo",
         description: [
           "Programación de maquinaria destinada a la almaceneria y manejo de cargas.",
-          "Desarrollo de aplicaciones en Step 7 y entornos de usuario en WinCC Flexible.",
+          "Desarrollo de aplicaciones de automatización.",
         ],
       },
       {
@@ -127,7 +126,7 @@ export const Data = {
         company: "Mecalux S.A",
         description: [
           "Programación de maquinaria destinada a la almaceneria y manejo de cargas.",
-          "Desarrollo de aplicaciones en Step 7 y entornos de usuario en WinCC Flexible.",
+          "Desarrollo de aplicaciones de automatización.",
           "Desarrollo de aplicaciones y entornos de usuario en soft-plc propietario.",
           "Servicio de asistencia post-venta.",
         ],
@@ -146,7 +145,8 @@ export const Data = {
         company: "GH Induction",
         period: "2025.6 - 2025.9",
         description: [
-          "Máquina de temple de componentes para automoción.",          
+          "Máquina de temple de componentes para automoción.",  
+          "- #TIA Portal V19, #SQL, #Sinamics S120, #PROFINET.",        
         ],
       },    
       {
@@ -154,7 +154,8 @@ export const Data = {
         company: "GH Induction",
         period: "2025.1 - 2025.9",
         description: [
-          "Máquina de temple de componentes para automoción.",          
+          "Máquina de temple de componentes para automoción.",     
+          "- #TIA Portal V19, #SQL, #Sinamics S120, #PROFINET, #Safety integrated.",             
         ],
       },      
       {
@@ -163,6 +164,7 @@ export const Data = {
         period: "2024.6 - 2024.12",
         description: [
           "Máquina de temple de ejes.",          
+          "- #TIA Portal V18, #Sinamics V90, #PROFINET",
         ],
       },
       {
@@ -170,7 +172,8 @@ export const Data = {
         company: "GH Induction",
         period: "2024.1 - 2024.6",
         description: [
-          "Máquina de temple de componentes para automoción.",       
+          "Máquina de temple de componentes para automoción.",    
+          "- #Studio 5000, #FactoryTalk, #Kinetix 5500, #Ethernet/IP",   
         ],
       },
       {
@@ -178,7 +181,8 @@ export const Data = {
         company: "GH Induction",
         period: "2023.6 - 2023.12",
         description: [          
-          "Desarrollo de aplicación de monitorización de procesos de inducción.",          
+          "Desarrollo de aplicación de monitorización de procesos de inducción.", 
+          "- #.NET, #C#, #WPF, #MVVM, #OPC-UA, #SQL.",         
         ],
       },      
       {
@@ -187,7 +191,7 @@ export const Data = {
         period: "2023.1 - 2023.5",
         description: [
           "Máquina de temple y revenido de componentes para automoción.",          
-          "Desarrollo de estandar de control basado en ISA-S88 para controladores SIMATIC S7-1500.",
+          "- #TIA Portal V18, #Sinamics V90, #PROFINET, ISA S88.",      
         ],
       },      
       {
@@ -196,6 +200,7 @@ export const Data = {
         period: "2022.6 - 2022.12",
         description: [
           "Máquina de temple y revenido de componentes para automoción.",          
+          "- #TIA Portal V18, #Sinamics V90, #PROFINET.",      
         ],
       },      
       {
@@ -204,30 +209,43 @@ export const Data = {
         period: "2022.5 - 2022.6",
         description: [
           "Desarrollo de interfaz de comunicaciones e integración de nueva carta de control de generadores para controladores Rockwell.",          
+          "- #Studio 5000, #FactoryTalk.",
         ],
       },
       {
         name: "Zhengyi - China",
         company: "GH Induction",
-        period: "2021.9 - 2022.4",
+        period: "2021.12 - 2022.4",
         description: [
-          "Máquina de temple y revenido de componentes para automoción.",          
+          "Máquina de temple y revenido de componentes para automoción.",   
+          "- #TIA Portal V17, #Sinamics V90, #PROFINET.",       
         ],
       },
       {
         name: "Valourec - Francia",
         company: "GH Induction",
-        period: "2021.7 - 2021.9",
+        period: "2021.8 - 2021.12",
         description: [
-          "Máquina de stress relief y swaging de tubos.",          
+          "Máquina de stress relief y swaging de tubos.",   
+          "- #Studio 5000, #FactoryTalk, #Kinetix 5500, #Ethernet/IP",       
         ],
-      },      
+      },     
+      {
+        name: " - Argentina",
+        company: "GH Induction",
+        period: "2021.6 - 2021.8",
+        description: [
+          "Máquina de stress relief y swaging de tubos.",,  
+          "- #TIA Portal V17, #WinCC Unified.",        
+        ],
+      }, 
       {
         name: "CIE Amaya - México",
         company: "GH Induction",
         period: "2021.4 - 2021.6",
         description: [
-          "Máquina de temple y revenido de componentes para automoción.",          
+          "Máquina de temple y revenido de componentes para automoción.",  
+          "- #TIA Portal V17, #Sinamics V90, #CU-320, #PROFINET.",        
         ],
       },
       {
@@ -235,7 +253,8 @@ export const Data = {
         company: "Órbita Ingeniería S.L.",
         period: "2019.3 - 2019.9",
         description: [
-          "Retrofit de proceso de cataforesis.",          
+          "Retrofit de proceso de cataforesis.",  
+          "- #TIA Portal V15.1, #WinCC, #RSLogix 5, #PROFINET, #Safety integrated.",        
         ],
       },
       {
@@ -244,6 +263,7 @@ export const Data = {
         period: "2014.4 - 2019.3",
         description: [
           "Desarrollo y puesta en marcha de diversas máquinas de montaje, mecanizado y test de fugas para la planta de fabricación de motores.",          
+          "- #TIA Portal V13, V14, V15.1, #WinCC, #RSLogix 5, #RSLogix 500, #RSLogix 5000, #PROFINET, #Ethernet/IP, #Safety integrated, #C#, #Cognex 5000/7000, #Yamaha RCX480.",
         ],
       },
       {
@@ -251,7 +271,8 @@ export const Data = {
         company: "Autónomo (Mecalux S.A.)",
         period: "2013.4 - 2014.4",
         description: [
-          "Desarrollo y puesta en marcha de grandes cargas.",          
+          "Desarrollo y puesta en marcha de grandes cargas.",      
+          "- #Soft-plc propietario.",    
         ],
       },
       {
@@ -259,7 +280,8 @@ export const Data = {
         company: "Autónomo (Mecalux S.A.)",
         period: "2012.9 - 2013.4",
         description: [
-          "Desarrollo y puesta en marcha de grandes cargas.",          
+          "Desarrollo y puesta en marcha de grandes cargas.", 
+          "- #Step 7, #WinCC flexible, #Siemens S7-300, #Siemens S7-400, #Sinamics G120, #PROFINET.",        
         ],
       },
       {
@@ -267,7 +289,8 @@ export const Data = {
         company: "Mecalux S.A.",
         period: "2011.10 - 2011.12",
         description: [
-          "Desarrollo y puesta en marcha de almacén vertical.",          
+          "Desarrollo y puesta en marcha de almacén vertical.", 
+          "- #Soft-plc propietario.",             
         ],
       },
       {
@@ -275,7 +298,8 @@ export const Data = {
         company: "Mecalux S.A.",
         period: "2011.5 - 2011.10",
         description: [
-          "Retrofit de almacén automático de grandes cargas.",          
+          "Retrofit de almacén automático de grandes cargas.",     
+          "- #Step 7, #WinCC flexible, #Siemens S7-300, #Sinamics G120, #PROFINET.",             
         ],
       },      
       {
@@ -283,7 +307,8 @@ export const Data = {
         company: "Mecalux-Interlake",
         period: "2010.9 - 2011.5",
         description: [
-          "Desarrollo y puesta en marcha de almacén automático de grandes cargas.",          
+          "Desarrollo y puesta en marcha de almacén automático de grandes cargas.",      
+          "- #Soft-plc propietario, #RSLogix 500, #Visual Basic.",                 
         ],
       },       
       {
@@ -291,7 +316,8 @@ export const Data = {
         company: "Mecalux S.A.",
         period: "2009.8 - 2010.9",
         description: [
-          "Desarrollo y puesta en marcha de almacén automático de grandes cargas.",          
+          "Desarrollo y puesta en marcha de almacén automático de grandes cargas.",     
+          "- #Soft-plc propietario.",                  
         ],
       },
       {
@@ -300,6 +326,7 @@ export const Data = {
         period: "2008.10 - 2009.8",
         description: [
           "Desarrollo y puesta en marcha de almacén automático de grandes cargas.",
+          "- #Soft-plc propietario.",             
         ],
       },
       {
@@ -308,6 +335,7 @@ export const Data = {
         period: "2008.8 - 2008.10",
         description: [
           "Desarrollo y puesta en marcha de buffer de macerado para productos alimenticios.",
+          "- #Soft-plc propietario.",             
         ],
       },
       {
@@ -315,7 +343,8 @@ export const Data = {
         company: "Mecalux S.A.",
         period: "2008.1 - 2008.7",
         description: [
-          "Desarrollo y puesta en marcha de almacén automático de grandes cargas.",          
+          "Desarrollo y puesta en marcha de almacén automático de grandes cargas.",       
+          "- #Soft-plc propietario.",                
         ],
       },
       {
@@ -324,6 +353,7 @@ export const Data = {
         period: "2007.10 - 2007.12",
         description: [
           "Puesta en marcha de almacén automático de pequeñas cargas.",
+          "- #Soft-plc propietario.",             
         ],
       },                     
     ],
