5678fa5c6b7ad10d8d747fb148529013c31ad0f7
Jesús Martín Fernández <txus.martin.fernandez@gmail.com>
2025-09-21 03:18:03 +0200

    update cv data

--- src/Schemas/Data.js
+++ src/Schemas/Data.js
@@ -25,7 +25,7 @@ export const Data = {
       "FactoryTalk", 
       "Phoenix Contact",
       "Omron/Yamaha RCX480",    
-      "Cognes 5000/7000",
+      "Cognex 5000/7000",
       "Codesys", 
       "OPC-UA",  
       ".NET",  
